6b5dd5d3eecee9fd98d24c02caf840a28da2dd88
SirRango <sturdivantdevon78@gmail.com>
2024-10-03 13:11:12 -0500

    added a timer

--- App.js
+++ App.js
@@ -34,12 +34,36 @@ export default function App() {
     return newDice;
   }
 
-  //useEffect() to track how many rolls the user has made
+  //useState() to track how many rolls the user has made
   const [numberOfRolls, setNumberOfRolls] = useState(0);
+  //useState() to track the time it took to win the game
+  const [timer, setTimer] = useState(0);
+  //state to check if the timer is running
+  const [isTimerRunning, setIsTimerRunning] = useState(false);
+  //the logic to track seconds
+  useEffect(() => {
+    let interval;
+    if (isTimerRunning) {
+      interval = setInterval(() => {
+        setTimer((prevTimer) => prevTimer + 1);
+      }, 1000);
+    }
+    //checking if the user has one if so turning of the timer
+    if (tenzies) {
+      setIsTimerRunning(false);
+    }
+
+    return () => clearInterval(interval);
+  }, [isTimerRunning, tenzies]);
 
   function rollDice() {
     //adding to the roll count each time the user rolls
     setNumberOfRolls((prevState) => prevState + 1);
+
+    //checking if the timer is running if not the setting it to true
+    if (!isTimerRunning) {
+      setIsTimerRunning(true);
+    }
 
     setDice((oldDice) => {
       //checking if the game is won
@@ -81,6 +105,7 @@ export default function App() {
           : `Roll until all dice are the same. Click each die to freeze it at its
         current value between rolls.`}
       </p>
+      {tenzies && <p>your time {timer} seconds</p>}
       <div className="dice-container">{diceElements}</div>
       {tenzies === true && <Confetti />}
       <button className="roll-dice" onClick={rollDice}>
